315bf1b6971b01572efdf04420dfeb00398bf9e0
yedu.k <yedu345@gmail.com>
2025-11-24 15:12:38 +0530

    app

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,16 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://ten-bay-beta.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
+  });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(3000);
 }
