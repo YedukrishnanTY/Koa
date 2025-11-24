5fe5f10fe1489d6e2947d11fd1a534a3e186940f
yedu.k <yedu345@gmail.com>
2025-11-24 13:41:39 +0530

    vercel deploy issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,8 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const express = require('express');
-const serverless = require('serverless-http');
-
-const expressApp = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.init();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-bootstrap().catch(err => {
-  console.error('Nest bootstrap error', err);
-});
-
-export const handler = serverless(expressApp);
-
+bootstrap();
