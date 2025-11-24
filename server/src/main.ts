180673e027f41fcebb32612d3331c260ce6566fc
yedu.k <yedu345@gmail.com>
2025-11-24 15:17:53 +0530

    methods

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,15 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
   app.enableCors({
     origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   await app.listen(3000);
