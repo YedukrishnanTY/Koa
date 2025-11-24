8000795a127db64408cc2d158eab4dfebcf93a10
yedu.k <yedu345@gmail.com>
2025-11-24 12:49:24 +0530

    bcrypt

--- server/src/service/auth.service.ts
+++ server/src/service/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import bcrypt from "node_modules/bcryptjs";
+import bcrypt from "bcryptjs";
 import { Auth, AuthDocument } from "../schemas/auth.schemas";
 
 @Injectable()
