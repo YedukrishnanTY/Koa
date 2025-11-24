df3b3150b3c4bef323aec0f43965e45eef346177
yedu.k <yedu345@gmail.com>
2025-11-24 14:54:23 +0530

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app'],
+    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
