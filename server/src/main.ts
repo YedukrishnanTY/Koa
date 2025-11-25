266a517463db29c5f1afcc68e7957b6caf173dd7
yedu.k <yedu345@gmail.com>
2025-11-25 20:34:35 +0530

    domain cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app', 'https://finx.is-a.dev/'],
+    origin: ['http://localhost:3000', 'https://ten-bay-beta.vercel.app', 'https://www.fiin-x.online'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
